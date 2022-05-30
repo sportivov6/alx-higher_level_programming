1e5a2461e601fe2d874870e592db05decc631b82
sportivov6 <edmundmmatta@mail.com>
2022-05-30 13:36:52 -0700

    header file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,19 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stlib.h>
-#include <string.h>
-#include <stdio.h>
 
-typedef struct list_s
+typedef struct listint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
 #endif
